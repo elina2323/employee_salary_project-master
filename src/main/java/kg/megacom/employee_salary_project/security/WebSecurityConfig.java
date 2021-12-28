6db9af387ce24fba75c427121adb67521d7b810f
qwerty123 <elina.balabaeva23@gmail.com>
2021-12-28 17:00:08 +0600

    commit config

---
+++ src/main/java/kg/megacom/employee_salary_project/security/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package kg.megacom.employee_salary_project.security;
+
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter implements ApplicationContextAware {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable();
+
+    }
+}
